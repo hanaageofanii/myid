1ff3088e031a9f77ca3f85ad3f569f90709ab72e
hanageofani <hanaageofanii@gmail.com>
2025-02-20 16:32:21 +0700

    Add background panel admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Filament\Support\Facades\FilamentView;
+use Illuminate\Support\Facades\Blade;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FilamentView::registerRenderHook(
+            'panels::auth.login.form.after',
+            fn(): string => Blade::render('@vite(\'resources/css/custom-login.css\')'),
+        );
     }
 }
