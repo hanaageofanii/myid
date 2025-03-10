285841ca062e01ecc8588810cd52965f1c800d2d
hanageofani <hanaageofanii@gmail.com>
2025-03-10 14:53:00 +0700

    Add label formLegal

--- app/Filament/Resources/FormLegalResource.php
+++ app/Filament/Resources/FormLegalResource.php
@@ -33,48 +33,49 @@ class FormLegalResource extends Resource
     {
         return $form
             ->schema([
+                    Forms\Components\Select::make('siteplan')
+                        ->label('Blok')
+                        ->nullable()
+                        ->options(fn ($get, $set, $record) => 
+                            form_kpr::where('status_akad', 'akad') 
+                                ->pluck('siteplan', 'siteplan')
+                                ->toArray()
+                            + ($record?->siteplan ? [$record->siteplan => $record->siteplan] : [])
+                        )
+                        ->reactive()
+                        ->afterStateUpdated(function ($state, callable $set) {
+                            $gcv = form_kpr::where('siteplan', $state)->first(); 
 
-Forms\Components\Select::make('siteplan')
-    ->nullable()
-    ->options(fn ($get, $set, $record) => 
-        form_kpr::where('status_akad', 'akad') // Gunakan model Eloquent
-            ->pluck('siteplan', 'siteplan')
-            ->toArray()
-        + ($record?->siteplan ? [$record->siteplan => $record->siteplan] : [])
-    )
-    ->reactive()
-    ->afterStateUpdated(function ($state, callable $set) {
-        $gcv = form_kpr::where('siteplan', $state)->first(); // Gunakan model Eloquent
+                            if ($gcv) {
+                                $set('nama_konsumen', $gcv->nama_konsumen);
+                            }
+                        }),
 
-        if ($gcv) {
-            $set('nama_konsumen', $gcv->nama_konsumen);
-        }
-    }),
+                        Forms\Components\TextInput::make('nama_konsumen')->nullable()->label('Nama Konsumen'),
 
-Forms\Components\TextInput::make('nama_konsumen')->nullable(),
-
-                        Forms\Components\TextInput::make('id_rumah')->nullable(),
+                        Forms\Components\TextInput::make('id_rumah')->nullable()->label('No. ID Rumah'),
 
                         Forms\Components\Select::make('status_sertifikat')
+                        ->label('Status Sertifikat')
                         ->options([
                             'induk' => 'Induk',
                             'pecahan' => 'Pecahan',
                         ])->nullable(),
 
-                        Forms\Components\TextInput::make('no_sertifikat')->nullable(),
-                        Forms\Components\TextInput::make('nib')->nullable(),
+                        Forms\Components\TextInput::make('no_sertifikat')->nullable()->label('No. Sertifikat'),
+                        Forms\Components\TextInput::make('nib')->nullable()->label('NIB'),
 
-                        Forms\Components\TextInput::make('luas_sertifikat')->nullable(),
-                        Forms\Components\TextInput::make('imb_pbg')->nullable(),
+                        Forms\Components\TextInput::make('luas_sertifikat')->nullable()->label('Luas Sertifikat'),
+                        Forms\Components\TextInput::make('imb_pbg')->nullable()->label('IMB/PBG'),
 
-                        Forms\Components\TextInput::make('nop')->nullable(),
-                        Forms\Components\TextInput::make('nop1')->nullable(),
+                        Forms\Components\TextInput::make('nop')->nullable()->label('NOP'),
+                        Forms\Components\TextInput::make('nop1')->nullable()->label('NOP Tambahan'),
 
                     Forms\Components\Fieldset::make('Dokumen')
                     ->schema([
-                        Forms\Components\FileUpload::make('up_sertifikat')->disk('public')->nullable(),
-                        Forms\Components\FileUpload::make('up_pbb')->disk('public')->nullable(),
-                        Forms\Components\FileUpload::make('up_img')->disk('public')->nullable(),
+                        Forms\Components\FileUpload::make('up_sertifikat')->disk('public')->nullable()->label('Dokumen Sertifikat'),
+                        Forms\Components\FileUpload::make('up_pbb')->disk('public')->nullable()->label('Dokumen PBB'),
+                        Forms\Components\FileUpload::make('up_img')->disk('public')->nullable()->label('Dokumen IMG'),
                     ]),                        
             ]);
     }
@@ -83,7 +84,7 @@ Forms\Components\TextInput::make('nama_konsumen')->nullable(),
     {
         return $table
             ->columns([
-                //
+                
             ])
             ->filters([
                 //
