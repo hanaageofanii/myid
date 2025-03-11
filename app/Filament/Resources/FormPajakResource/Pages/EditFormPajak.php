16ec5da8ef3c28470b7ab858f841d346a736b05d
hanageofani <hanaageofanii@gmail.com>
2025-03-11 11:44:28 +0700

    Edit Pages EditFormPajak.php

--- app/Filament/Resources/FormPajakResource/Pages/EditFormPajak.php
+++ app/Filament/Resources/FormPajakResource/Pages/EditFormPajak.php
@@ -10,10 +10,20 @@ class EditFormPajak extends EditRecord
 {
     protected static string $resource = FormPajakResource::class;
 
+    protected static ?string $title = "Ubah Data Validasi PPH";
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->label('Hapus Data Validasi PPH'),
         ];
     }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
+    
 }
