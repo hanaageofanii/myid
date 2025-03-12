5b98ce5be03413fcd756cd8de2cc4c1a01935313
hanageofani <hanaageofanii@gmail.com>
2025-03-12 14:10:31 +0700

    Add List Form PPN

--- app/Filament/Resources/FormPpnResource/Pages/ListFormPpns.php
+++ app/Filament/Resources/FormPpnResource/Pages/ListFormPpns.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\FormPpnResource\Pages;
 
 use App\Filament\Resources\FormPpnResource;
+use App\Filament\Resources\FormPpnResource\Widgets\PPNStats;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -10,10 +11,25 @@ class ListFormPpns extends ListRecords
 {
     protected static string $resource = FormPpnResource::class;
 
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Faktur Pajak'),
         ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            PPNStats::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
