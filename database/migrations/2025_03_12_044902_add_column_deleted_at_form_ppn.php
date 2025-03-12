bfa7427c4162e3ec9e6dd15e540a392ebdf1be7e
hanageofani <hanaageofanii@gmail.com>
2025-03-12 14:13:49 +0700

    Add Soft Deleted

--- database/migrations/2025_03_12_044902_add_column_deleted_at_form_ppn.php
+++ database/migrations/2025_03_12_044902_add_column_deleted_at_form_ppn.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('form_ppn', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
