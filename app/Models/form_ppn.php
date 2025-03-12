28318d552ce7d4c1658acec36d2369bf6ef286ad
hanageofani <hanaageofanii@gmail.com>
2025-03-12 14:18:20 +0700

    Add protected fillable

--- app/Models/form_ppn.php
+++ app/Models/form_ppn.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class form_ppn extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "siteplan",
+        "kavling",
+        "nama_konsumen",
+        "nik",
+        "npwp",
+        "alamat",
+        "no_seri_faktur",
+        "tanggal_faktur",
+        "harga_jual",
+        "dpp_ppn",
+        "tarif_ppn",
+        "jumlah_ppn",
+        "status",
+        "tanggal_bayar_ppn",
+        "ntpn_ppn",
+        "up_bukti_setor_ppn",
+        "up_efaktur",
+    ];
 }
