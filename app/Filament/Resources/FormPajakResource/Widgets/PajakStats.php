9e34391bddb17c7d179c146a5a07692e19c14bc8
hanageofani <hanaageofanii@gmail.com>
2025-03-12 10:58:47 +0700

    Add PajakStats

--- app/Filament/Resources/FormPajakResource/Widgets/PajakStats.php
+++ app/Filament/Resources/FormPajakResource/Widgets/PajakStats.php
@@ -2,15 +2,56 @@
 
 namespace App\Filament\Resources\FormPajakResource\Widgets;
 
+use App\Models\form_pajak;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
+use Filament\Widgets\StatsOverviewWidget\Card;
+
 class PajakStats extends BaseWidget
 {
+
+    
+    protected static ?int $sort = 5;
+    protected static ?string $maxHeight = '300px';
+    protected static ?string $pollingInterval = '10s';
+    protected static bool $isLazy = false;
+    protected ?string $heading = 'Dashboard Validasi';
     protected function getStats(): array
     {
         return [
-            //
+            Card::make('Total Data Sertifikat', form_pajak::count())
+            ->extraAttributes([
+                'style' => 'background-color: #FFC107; border-color: #234C63;'
+            ]), 
+            Card::make('Total Site Plan', form_pajak::distinct('siteplan')->count('siteplan'))
+            ->extraAttributes([
+                'style' => 'background-color: #FFC107; border-color: #234C63;'
+            ]),  
+            Card::make('Jumlah Validasi Unit Standar', form_pajak::where('kavling', 'standar')->count())
+            ->extraAttributes([
+                'style' => 'background-color:#FFC107; form_pajak; border-color: #234C63;'
+            ]),            
+            Card::make('Jumlah Validasi Unit Khusus', form_pajak::where('kavling', 'khusus')->count())
+            ->extraAttributes([
+                'style' => 'background-color:#FFC107; form_pajak; border-color: #234C63;'
+            ]),
+            Card::make('Jumlah Validasi Unit Hook', form_pajak::where('kavling', 'hook')->count())
+            ->extraAttributes([
+                'style' => 'background-color: #FFC107;form_pajak; border-color: #234C63;'
+            ]),
+            Card::make('Jumlah Validasi Unit Komersil', form_pajak::where('kavling', 'komersil')->count())
+            ->extraAttributes([
+                'style' => 'background-color:#FFC107; form_pajak; border-color: #234C63;'
+            ]),
+            Card::make('Jumlah Validasi Unit Tanah Lebih', form_pajak::where('kavling', 'tanah_lebih')->count())
+            ->extraAttributes([
+                'style' => 'background-color:#FFC107; form_pajak; border-color: #234C63;'
+            ]),
+            Card::make('Jumlah Validasi Unit Kios', form_pajak::where('kavling', 'kios')->count())
+            ->extraAttributes([
+                'style' => 'background-color:#FFC107; form_pajak; border-color: #234C63;'
+            ]),
         ];
     }
 }
