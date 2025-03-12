941fa01dc2d5e1dc11b3c00bb49564bb73e27804
hanageofani <hanaageofanii@gmail.com>
2025-03-12 14:25:30 +0700

    add database ppn

--- database/migrations/2025_03_12_044720_create_form_ppns_table.php
+++ database/migrations/2025_03_12_044720_create_form_ppns_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('form_ppns', function (Blueprint $table) {
             $table->id();
+            $table->string('siteplan')->nullable();
+            $table->string('kavling')->nullable();
+            $table->string('nama_konsumen')->nullable();
+            $table->string('nik')->nullable();
+            $table->string('npwp')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('no_seri_faktur')->nullable();
+            $table->date('tanggal_faktur')->nullable();
+            $table->string('harga_jual')->nullable();
+            $table->string('dpp_ppn')->nullable();
+            $table->string('tarif_ppn')->nullable();
+            $table->string('jumlah_ppn')->nullable();
+            $table->enum('status_ppn', ['dtp','dtp_sebagian','dibebaskan','bayar'])->nullable();
+            $table->date('tanggal_bayar_ppn')->nullable();
+            $table->string('ntpn_ppn')->nullable();
+
+            $table->string('up_bukti_setor_ppn')->nullable()->nullable();
+            $table->string('up_efaktur')->nullable()->nullable();
             $table->timestamps();
         });
     }
