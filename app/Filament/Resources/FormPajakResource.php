453c2c4d9e929951e058341bace921fcc94bde19
hanageofani <hanaageofanii@gmail.com>
2025-03-12 10:05:30 +0700

    Add table column

--- app/Filament/Resources/FormPajakResource.php
+++ app/Filament/Resources/FormPajakResource.php
@@ -15,6 +15,37 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\FormLegal;
+use App\Filament\Resources\GCVResource;
+use App\Models\GCV;
+use Illuminate\Support\Facades\Storage;
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Fieldset;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Enums\ActionsPosition;
+use Illuminate\Database\Eloquent\Collection;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\ActionGroup;
+// use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Grid;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Support\Enums\ActionSize;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use Filament\Notifications\Notification;
+use Filament\Tables\Filters\TrashedFilter;
 
 class FormPajakResource extends Resource
 {
@@ -124,7 +155,10 @@ class FormPajakResource extends Resource
                 Forms\Components\Select::make('tarif_pph')
                 ->label('Tarif PPH')
                 ->options(['1%' => '1 %', '2.5%' => '2.5 %'])
-                ->disabled(),
+                ->reactive()
+                ->afterStateUpdated(fn ($state, callable $set, callable $get) => 
+                    $set('jumlah_pph', max($get('harga') * ((float) rtrim($state, '%')) / 100, 0))
+                ),
 
                 Forms\Components\TextInput::make('jumlah_pph')->numeric()->nullable()->label('Jumlah PPH'),
                 Forms\Components\TextInput::make('kode_billiing_pph')->numeric()->nullable()->label('Kode Billing PPH'),
@@ -150,7 +184,41 @@ class FormPajakResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('siteplan')->sortable()->searchable()->label('Blok'),
+                Tables\Columns\TextColumn::make('no_sertifikat')->sortable()->searchable()->label('No. Sertifikat'),
+                Tables\Columns\TextColumn::make('kavling')->sortable()->searchable()->label('Jenis Unit'),
+                Tables\Columns\TextColumn::make('nama_konsumen')->sortable()->searchable()->label('Nama Konsumen'),
+                Tables\Columns\TextColumn::make('nik')->sortable()->searchable()->label('NIK'),
+                Tables\Columns\TextColumn::make('npwp')->sortable()->searchable()->label('NPWP'),
+                Tables\Columns\TextColumn::make('alamat')->sortable()->searchable()->label('Alamat'),
+                Tables\Columns\TextColumn::make('nop')->sortable()->searchable()->label('NOP'),
+                Tables\Columns\TextColumn::make('luas_tanah')->sortable()->searchable()->label('Luas Sertifikat'),
+                Tables\Columns\TextColumn::make('harga')->sortable()->searchable()->label('Harga'),
+                Tables\Columns\TextColumn::make('npoptkp')->sortable()->searchable()->label('NPOPTKP'),
+                Tables\Columns\TextColumn::make('jumlah_bphtb')->sortable()->searchable()->label('Jumlah BPHTB'),
+                Tables\Columns\TextColumn::make('tarif_pph')->sortable()->searchable()->label('Tarif PPH'),
+                Tables\Columns\TextColumn::make('jumlah_pph')->sortable()->searchable()->label('Jumlah PPH'),
+                Tables\Columns\TextColumn::make('kode_billing_pph')->sortable()->searchable()->label('Kode Billing PPH'),
+                Tables\Columns\TextColumn::make('tanggal_bayar_pph')->sortable()->searchable()->label('Tanggal Bayar PPH'),
+                Tables\Columns\TextColumn::make('ntpnpph')->sortable()->searchable()->label('NTPN PPH'),
+                Tables\Columns\TextColumn::make('validasi_pph')->sortable()->searchable()->label('Validasi PPH'),
+                Tables\Columns\TextColumn::make('tanggal_validasi')->sortable()->searchable()->label('Tanggal validasi'),
+
+                Tables\Columns\TextColumn::make('up_kode_billing')
+                ->label('Dokumen Kode Billing')
+                ->url(fn ($record) => $record->up_sertifikat ? Storage::url($record->up_sertifikat) : '#', true)
+                ->sortable()
+                ->searchable(),
+                Tables\Columns\TextColumn::make('up_bukti_setor_pajak')
+                ->label('Dokumen Bukti Setor Pajak')
+                ->url(fn ($record) => $record->up_pbb ? Storage::url($record->up_pbb) : '#', true)
+                ->sortable()
+                ->searchable(),
+                Tables\Columns\TextColumn::make('up_suket_validasi')
+                ->label('Dokumen Suket Validasi')
+                ->url(fn ($record) => $record->up_img ? Storage::url($record->up_img) : '#', true)
+                ->sortable()
+                ->searchable(),
             ])
             ->filters([
                 //
