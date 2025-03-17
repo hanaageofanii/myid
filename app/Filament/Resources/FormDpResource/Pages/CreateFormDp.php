ace3a0db71f5ca716b0402c02d26008a38e0a3b5
hanageofani <hanaageofanii@gmail.com>
2025-03-17 11:16:38 +0700

    Add create form dp

--- app/Filament/Resources/FormDpResource/Pages/CreateFormDp.php
+++ app/Filament/Resources/FormDpResource/Pages/CreateFormDp.php
@@ -5,8 +5,37 @@ namespace App\Filament\Resources\FormDpResource\Pages;
 use App\Filament\Resources\FormDpResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateFormDp extends CreateRecord
 {
     protected static string $resource = FormDpResource::class;
+
+    protected static ?string $title = "Tambah Data Uang Muka";
+    protected function getCreateFormAction(): Actions\Action
+    {
+        return parent::getCreateFormAction()
+        ->label('Tambah Data');
+    }
+
+    protected function getCreateAnotherFormAction(): Actions\Action
+    {
+        return parent::getCreateAnotherFormAction()
+        ->label('Tambah Data Lagi')
+        ->color('warning');
+    }
+    
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Data Disimpan')
+            ->body('Data telah berhasil disimpan.');
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
 }
