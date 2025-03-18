3f67d124fb8d61332d897d1b366fdb5c0be49136
hanageofani <hanaageofanii@gmail.com>
2025-03-18 14:25:21 +0700

    Add model

---
+++ app/Models/pencairan_dajam.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pencairan_dajam extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/pengajuan_dajam.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pengajuan_dajam extends Model
+{
+    use HasFactory;
+}
