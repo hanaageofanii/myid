0f2274950a47298294e1eadfcfb93f6ba71b2db0
hanageofani <hanaageofanii@gmail.com>
2025-03-19 10:49:26 +0700

    editCreatePages Pencairan akad

--- app/Filament/Resources/PencairanAkadResource/Pages/CreatePencairanAkad.php
+++ app/Filament/Resources/PencairanAkadResource/Pages/CreatePencairanAkad.php
@@ -5,8 +5,46 @@ namespace App\Filament\Resources\PencairanAkadResource\Pages;
 use App\Filament\Resources\PencairanAkadResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+
 
 class CreatePencairanAkad extends CreateRecord
 {
     protected static string $resource = PencairanAkadResource::class;
+    protected static ?string $title = "Buat Data Pencairan";
+    protected function getCreateFormAction(): Actions\Action
+    {
+        return parent::getCreateFormAction()
+        ->label('Tambah Data');
+    }
+
+    protected function getCreateAnotherFormAction(): Actions\Action
+    {
+        return parent::getCreateAnotherFormAction()
+        ->label('Tambah Data Lagi')
+        ->color('warning');
+    }
+    
+    protected function getCancelFormAction() : Actions\Action
+    {
+        return parent::getCancelFormAction()
+        ->label('Batal')
+        ->color('danger');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Data Pencairan Disimpan')
+            ->body('Data Pencairan telah berhasil disimpan.');
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
 }
+
+
