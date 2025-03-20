b610e7888a8e60213ed975c5416ef886e5dcf912
hanageofani <hanaageofanii@gmail.com>
2025-03-20 14:29:32 +0700

    add card

--- app/Filament/Resources/AjbResource/Widgets/AjbStats.php
+++ app/Filament/Resources/AjbResource/Widgets/AjbStats.php
@@ -4,13 +4,29 @@ namespace App\Filament\Resources\AjbResource\Widgets;
 
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use App\Models\ajb;
+
 
 class AjbStats extends BaseWidget
 {
+    protected static ?int $sort = 9;
+
+    protected static ?string $maxHeight = '300px';
+    protected static ?string $pollingInterval = '10s';
+    protected static bool $isLazy = false;
+    protected ?string $heading = 'Dashboard AJB';
+    
     protected function getStats(): array
     {
         return [
-            //
-        ];
+            Card::make('Total Data Audit', ajb::count())
+            ->extraAttributes([
+                'style' => 'background-color: #85c1e9; border-color: #234C63;'
+            ]),    
+            Card::make('Total Site Plan', ajb::distinct('siteplan')->count('siteplan'))
+            ->extraAttributes([
+                'style' => 'background-color: #85c1e9; border-color: #234C63;'
+            ]),            ];
     }
 }
