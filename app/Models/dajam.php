d417d87354aa0aee427ebc8c041a9ed8a1d53520
hanageofani <hanaageofanii@gmail.com>
2025-03-21 09:42:00 +0700

    add dajam model

--- app/Models/dajam.php
+++ app/Models/dajam.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class dajam extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "siteplan",
+        "bank",
+        "no_debitur",
+        "nama_konsumen",
+        "max_kpr",
+        "nilai_pencairan",
+        "jumlah_dajam",
+        "dajam_sertifikat",
+        "dajam_imb",
+        "dajam_listrik",
+        "dajam_jkk",
+        "dajam_bestek",
+        "jumlah_realisasi_dajam",
+        "dajam_pph",
+        "dajam_bphtb",
+        "pembukuan",
+        "up_spd5",
+        "up_lainnya",
+    ];
 }
