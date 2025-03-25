2b3d80f99abcc987cf4a918e541da008186071e6
hanageofani <hanaageofanii@gmail.com>
2025-03-25 14:55:02 +0700

    add model pengajuan dajam

--- app/Models/pencairan_dajam.php
+++ app/Models/pencairan_dajam.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class pencairan_dajam extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+            'siteplan',
+            'bank',
+            'no_debitur',
+            'nama_konsumen',
+            'nama_dajam',
+            'nilai_dajam',
+            'tanggal_pencairan',
+            'nilai_pencairan',
+            'selisih_dajam',
+            'up_rekening_koran',
+            'up_lainnya',
+    ];
 }
