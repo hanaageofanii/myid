3b70c9babe351599f79d0d8c78869c4664b6410d
hanageofani <hanaageofanii@gmail.com>
2025-03-25 09:40:11 +0700

    migrasi

--- database/migrations/2025_03_25_022022_create_verifikasi_dajams_table.php
+++ database/migrations/2025_03_25_022022_create_verifikasi_dajams_table.php
@@ -13,6 +13,33 @@ return new class extends Migration
     {
         Schema::create('verifikasi_dajams', function (Blueprint $table) {
             $table->id();
+            $table->string('siteplan')->nullable();
+            $table->enum('bank',['btn_cikarang','btn_bekasi','btn_karawang','bjb_syariah','bjb_jababeka','btn_syariah','brii_bekasi'])->nullable();
+            $table->string('no_debitur')->nullable();
+            $table->string('nama_konsumen')->nullable();
+            $table->string('max_kpr')->nullable();
+            $table->string('nilai_pencairan')->nullable();
+            $table->string('total_dajam')->nullable();
+            $table->string('dajam_sertifikat')->nullable();
+            $table->string('dajam_imb')->nullable();
+            $table->string('dajam_listrik')->nullable();
+            $table->string('dajam_jkk')->nullable();
+            $table->string('dajam_bestek')->nullable();
+            $table->string('jumlah_realisasi_dajam')->nullable();
+            $table->string('dajam_pph')->nullable();
+            $table->string('dajam_bphtb')->nullable();
+            $table->string('pembukuan')->nullable();
+            $table->string('no_surat_pengajuan')->nullable();
+            $table->date('tgl_pencairan_dajam_sertifikat')->nullable();
+            $table->date('tgl_pencairan_dajam_imb')->nullable();
+            $table->date('tgl_pencairan_dajam_listrik')->nullable();
+            $table->date('tgl_pencairan_dajam_jkk')->nullable();
+            $table->date('tgl_pencairan_dajam_bester')->nullable();
+            $table->date('tgl_pencairan_dajam_pph')->nullable();
+            $table->date('tgl_pencairan_dajam_bphtb')->nullable();
+            $table->string('total_pencairan_dajam')->nullable();
+            $table->string('sisa_dajam')->nullable();
+            $table->enum('status_dajam',['sudah_diajukan','belum_diajukan'])->nullable();
             $table->timestamps();
         });
     }
