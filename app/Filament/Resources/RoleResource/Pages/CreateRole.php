58eb5ea2d5c6e3c82ca615abcebbed454f671d1c
hanageofani <hanaageofanii@gmail.com>
2025-04-10 16:57:59 +0700

    add CreateRole.php

--- app/Filament/Resources/RoleResource/Pages/CreateRole.php
+++ app/Filament/Resources/RoleResource/Pages/CreateRole.php
@@ -5,8 +5,46 @@ namespace App\Filament\Resources\RoleResource\Pages;
 use App\Filament\Resources\RoleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+
 
 class CreateRole extends CreateRecord
 {
     protected static string $resource = RoleResource::class;
+    protected static ?string $title = "Buat Role";
+    protected function getCreateFormAction(): Actions\Action
+    {
+        return parent::getCreateFormAction()
+        ->label('Tambah Data');
+    }
+
+    protected function getCreateAnotherFormAction(): Actions\Action
+    {
+        return parent::getCreateAnotherFormAction()
+        ->label('Tambah Data Lagi')
+        ->color('warning');
+    }
+    
+    protected function getCancelFormAction() : Actions\Action
+    {
+        return parent::getCancelFormAction()
+        ->label('Batal')
+        ->color('danger');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Role Disimpan')
+            ->body('Role telah berhasil disimpan.');
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
 }
+
+
