a0dc63eebb19aba5d8b38b0faf9c12a69b0347a6
hanageofani <hanaageofanii@gmail.com>
2025-04-10 16:59:15 +0700

    add editRole.php

--- app/Filament/Resources/RoleResource/Pages/EditRole.php
+++ app/Filament/Resources/RoleResource/Pages/EditRole.php
@@ -10,10 +10,24 @@ class EditRole extends EditRecord
 {
     protected static string $resource = RoleResource::class;
 
+    protected static ?string $title = "Ubah Role";
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->label('Hapus Role')
+            ->modalHeading(fn ($record) => "Konfirmasi Hapus {$record->name}")
+            ->modalDescription(fn ($record) => "Apakah Anda yakin ingin menghapus blok {$record->name}?"),
+
         ];
     }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
+    
 }
+
