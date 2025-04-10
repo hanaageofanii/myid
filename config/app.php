b930b832d628f56a3555c5149744fc745719458c
hanageofani <hanaageofanii@gmail.com>
2025-04-10 11:49:13 +0700

    add role admin

--- config/app.php
+++ config/app.php
@@ -169,6 +169,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ])->toArray(),
 
     /*
