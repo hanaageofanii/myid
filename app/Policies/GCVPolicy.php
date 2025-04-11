350095f5eb7eb9243ed8c8da330717a8eb2295a0
hanageofani <hanaageofanii@gmail.com>
2025-04-11 13:38:22 +0700

    add GCVPolicy

---
+++ app/Policies/GCVPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\GCV;
+use App\Models\User;
+
+class GCVPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasRole(['admin','Marketing','Super admin','Direksi','Staff','Staff Legal','Staff KPR','Legal Officer']);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, GCV $GCV): bool
+    {
+        return $user->hasRole(['admin','Marketing','Super admin','Direksi','Legal Officer']);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, GCV $GCV): bool
+    {
+        return $user->hasRole(['admin','Super admin','Direksi','Staff','Staff KPR','Staff Legal']);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, GCV $GCV): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, GCV $GCV): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, GCV $GCV): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+}
+
