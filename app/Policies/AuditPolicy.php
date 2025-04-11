5997e1052c9302f01f8029680217a4b4e933441d
hanageofani <hanaageofanii@gmail.com>
2025-04-11 13:27:22 +0700

    add ajb & audit policy

---
+++ app/Policies/AuditPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Audit;
+use App\Models\User;
+
+class AuditPolicy
+{
+    /**
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasRole(['admin','Marketing','Super admin','Direksi','Staff','Staff Legal','Staff KPR','Legal Officer']);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Audit $Audit): bool
+    {
+        return $user->hasRole(['admin','Marketing','Super admin','Direksi','Legal Officer']);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Audit $Audit): bool
+    {
+        return $user->hasRole(['admin','Super admin','Direksi','Staff','Staff KPR','Staff Legal']);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Audit $Audit): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Audit $Audit): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Audit $Audit): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+}
