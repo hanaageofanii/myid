8f45bac88cf528f4b143be3b37e89ae75a9d2b18
hanageofani <hanaageofanii@gmail.com>
2025-04-11 10:04:05 +0700

    edir createPermission.php

--- app/Filament/Resources/PermissionResource/Pages/CreatePermission.php
+++ app/Filament/Resources/PermissionResource/Pages/CreatePermission.php
@@ -5,11 +5,47 @@ namespace App\Filament\Resources\PermissionResource\Pages;
 use App\Filament\Resources\PermissionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+
 
 class CreatePermission extends CreateRecord
 {
     protected static string $resource = PermissionResource::class;
 
+    protected static ?string $title = "Buat Permission";
+    protected function getCreateFormAction(): Actions\Action
+    {
+        return parent::getCreateFormAction()
+        ->label('Tambah Data');
+    }
+
+    protected function getCreateAnotherFormAction(): Actions\Action
+    {
+        return parent::getCreateAnotherFormAction()
+        ->label('Tambah Data Lagi')
+        ->color('warning');
+    }
+    
+    protected function getCancelFormAction() : Actions\Action
+    {
+        return parent::getCancelFormAction()
+        ->label('Batal')
+        ->color('danger');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Permission Disimpan')
+            ->body('Permission telah berhasil disimpan.');
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
