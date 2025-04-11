935d031b890884b74ac0475222b5e6dcd91b1eee
hanageofani <hanaageofanii@gmail.com>
2025-04-11 13:37:19 +0700

    add form_ppnPolicy

---
+++ app/Policies/form_ppnPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\form_ppn;
+use App\Models\User;
+
+class form_ppnPolicy
+{
+   /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return $user->hasRole(['admin','Marketing','Super admin','Direksi','Staff','Staff Legal','Staff KPR','Legal Officer']);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user,form_ppn $form_ppn): bool
+    {
+        return $user->hasRole(['admin','Marketing','Super admin','Direksi','Legal Officer']);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user,form_ppn $form_ppn): bool
+    {
+        return $user->hasRole(['admin','Super admin','Direksi','Staff','Staff KPR','Staff Legal']);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user,form_ppn $form_ppn): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user,form_ppn $form_ppn): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user,form_ppn $form_ppn): bool
+    {
+        return $user->hasRole(['admin']);
+    }
+}
+
