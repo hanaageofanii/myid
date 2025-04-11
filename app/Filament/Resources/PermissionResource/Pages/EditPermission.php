a43bb5322881809daf2be1a8981118943c0f4803
hanageofani <hanaageofanii@gmail.com>
2025-04-11 10:06:08 +0700

    edit EditPermission.php

--- app/Filament/Resources/PermissionResource/Pages/EditPermission.php
+++ app/Filament/Resources/PermissionResource/Pages/EditPermission.php
@@ -10,17 +10,30 @@ class EditPermission extends EditRecord
 {
     protected static string $resource = PermissionResource::class;
 
+    protected static ?string $title = "Ubah Pemission";
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->label('Hapus Permission')
+            ->modalHeading(fn ($record) => "Konfirmasi Hapus {$record->name}")
+            ->modalDescription(fn ($record) => "Apakah Anda yakin ingin menghapus blok {$record->name}?"),
+
         ];
     }
 
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
+    
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
     }
+
 
     protected function getCreatedNotificationTitle(): ?string
     {
