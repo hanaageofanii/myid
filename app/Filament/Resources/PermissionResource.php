55491c3446913d525f828639797900e0f317d59e
hanageofani <hanaageofanii@gmail.com>
2025-04-11 09:59:07 +0700

    add permission resource

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -2,39 +2,44 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\RoleResource\Pages;
-use App\Filament\Resources\RoleResource\RelationManagers;
-// use App\Models\Role;
+use App\Filament\Resources\PermissionResource\Pages;
+use App\Filament\Resources\PermissionResource\RelationManagers;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Filament\Forms\Components\TextInput;
-use Filament\Tables\Columns\TextColumn;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Filament\Forms\Components\Card;
 
-class RoleResource extends Resource
+class PermissionResource extends Resource
 {
-    protected static ?string $model = Role::class;
+    protected static ?string $model = Permission::class;
 
-    protected static ?string $title = "Role";
+    protected static ?string $title = "Permission";
     protected static ?string $navigationGroup = "Settings";
-    protected static ?string $pluralLabel = "Role";
-    protected static ?string $navigationIcon = 'heroicon-o-user-group';
-    protected static ?string $navigationLabel = 'Role';
-    protected static ?string $pluralModelLabel = 'Role';
+    protected static ?string $pluralLabel = "Permission";
+    protected static ?string $navigationIcon = 'heroicon-o-cog-8-tooth';
+    protected static ?string $navigationLabel = 'Permission';
+    protected static ?string $pluralModelLabel = 'Permission';
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                TextInput::make('name')
+                Card::make()->schema([
+                    TextInput::make ('name')
                 ->minLength(2)
                 ->maxLength(255)
                 ->label('Nama')
-            ]);
+                ->required()
+                ->unique()
+            ])
+        ]);
     }
 
     public static function table(Table $table): Table
@@ -45,7 +50,7 @@ class RoleResource extends Resource
                 ->label('Nama'),
             ])
             ->filters([
-                
+                //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -60,16 +65,16 @@ class RoleResource extends Resource
     public static function getRelations(): array
     {
         return [
-            
+            //
         ];
     }
 
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ListRoles::route('/'),
-            'create' => Pages\CreateRole::route('/create'),
-            'edit' => Pages\EditRole::route('/{record}/edit'),
+            'index' => Pages\ListPermissions::route('/'),
+            'create' => Pages\CreatePermission::route('/create'),
+            'edit' => Pages\EditPermission::route('/{record}/edit'),
         ];
     }
 }
