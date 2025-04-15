bd011e729b9608428573ae7a743618e22c4d5e45
hanaageofanii <hanaageofanii@gmail.com>
2025-04-15 15:20:32 +0700

    add import

--- app/Filament/Resources/RekeningKoranResource.php
+++ app/Filament/Resources/RekeningKoranResource.php
@@ -13,6 +13,41 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\Rekonsil;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+use App\Models\form_dp;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Enums\ActionsPosition;
+use Illuminate\Database\Eloquent\Collection;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\Grid;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Support\Enums\ActionSize;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use Filament\Notifications\Notification;
+use Filament\Tables\Filters\TrashedFilter;
+
+use Filament\Tables\Actions\ForceDeleteAction;
 
 class RekeningKoranResource extends Resource
 {
@@ -28,7 +63,10 @@ class RekeningKoranResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Fieldset::make()
+                ->schema([
+
+                ])
             ]);
     }
 
