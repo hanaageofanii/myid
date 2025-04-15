96f967ce3357216c95a51927a893058663865d28
hanaageofanii <hanaageofanii@gmail.com>
2025-04-15 15:10:18 +0700

    add pages EditRekeningKoran.php

--- app/Filament/Resources/RekeningKoranResource/Pages/EditRekeningKoran.php
+++ app/Filament/Resources/RekeningKoranResource/Pages/EditRekeningKoran.php
@@ -10,10 +10,23 @@ class EditRekeningKoran extends EditRecord
 {
     protected static string $resource = RekeningKoranResource::class;
 
+    protected static ?string $title = "Ubah Data Rekening Koran";
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->label('Hapus Rekening Koran')
+            ->modalHeading(fn ($record) => "Konfirmasi Hapus {$record->no_transakasi}")
+            ->modalDescription(fn ($record) => "Apakah Anda yakin ingin menghapus blok {$record->no_transaksi}?"),
+
         ];
     }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
+    
 }
