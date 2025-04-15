c6332d041ca2de4cba19302ba68a0bfb828c3b41
hanaageofanii <hanaageofanii@gmail.com>
2025-04-15 11:05:20 +0700

    edit EditRekonsil.php

--- app/Filament/Resources/RekonsilResource/Pages/EditRekonsil.php
+++ app/Filament/Resources/RekonsilResource/Pages/EditRekonsil.php
@@ -10,10 +10,23 @@ class EditRekonsil extends EditRecord
 {
     protected static string $resource = RekonsilResource::class;
 
+    protected static ?string $title = "Ubah Data Transaksi Internal";
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->label('Hapus Transaksi Internal Dajam')
+            ->modalHeading(fn ($record) => "Konfirmasi Hapus {$record->no_transakasi}")
+            ->modalDescription(fn ($record) => "Apakah Anda yakin ingin menghapus blok {$record->no_transaksi}?"),
+
         ];
     }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
+    
 }
