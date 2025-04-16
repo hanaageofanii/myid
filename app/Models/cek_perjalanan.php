7884fdc75d378632e60d54234ec45ee442250dd2
hanaageofanii <hanaageofanii@gmail.com>
2025-04-16 13:58:17 +0700

    add model in cek perjalanan

--- app/Models/cek_perjalanan.php
+++ app/Models/cek_perjalanan.php
@@ -9,4 +9,21 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class cek_perjalanan extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        
+            'no_ref_bank',            
+            'no_transaksi',            
+            'nama_pencair',            
+            'tanggal_dicairkan',
+            'nama_penerima',            
+            'tanggal_diterima',            
+            'tujuan_dana',
+            'status_disalurkan',        
+            'bukti_pendukung'
+    ];
+
+    protected $casts = [
+        "bukti_pendukung" => 'array',
+    ];
 }
