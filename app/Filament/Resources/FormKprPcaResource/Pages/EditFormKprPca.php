915421230103de9e29529c7774f53e580f49038b
hanaageofanii <hanaageofanii@gmail.com>
2025-04-29 16:05:30 +0700

    modified page

--- app/Filament/Resources/FormKprPcaResource/Pages/EditFormKprPca.php
+++ app/Filament/Resources/FormKprPcaResource/Pages/EditFormKprPca.php
@@ -10,10 +10,24 @@ class EditFormKprPca extends EditRecord
 {
     protected static string $resource = FormKprPcaResource::class;
 
+    protected static ?string $title = "Ubah Data Penjualan";
+
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->label('Hapus Data KPR')
+            ->modalHeading(fn ($record) => "Konfirmasi Hapus {$record->siteplan}")
+            ->modalDescription(fn ($record) => "Apakah Anda yakin ingin menghapus blok {$record->siteplan}?"),
+
+
         ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
--- app/Filament/Resources/FormKprPcaResource/Pages/ListFormKprPcas.php
+++ app/Filament/Resources/FormKprPcaResource/Pages/ListFormKprPcas.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\FormKprPcaResource\Pages;
 use App\Filament\Resources\FormKprPcaResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\FormKprPcaResource\Widgets\form_kpr_pca;
+
 
 class ListFormKprPcas extends ListRecords
 {
@@ -13,7 +15,21 @@ class ListFormKprPcas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Data KPR'),
         ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            form_kpr_pca::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
