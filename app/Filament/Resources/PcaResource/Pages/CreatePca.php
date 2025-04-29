347c502ffdaff11e78663dc0d548ce57137f3ea0
hanaageofanii <hanaageofanii@gmail.com>
2025-04-29 15:27:14 +0700

    modified pages

--- app/Filament/Resources/PcaResource/Pages/CreatePca.php
+++ app/Filament/Resources/PcaResource/Pages/CreatePca.php
@@ -5,8 +5,46 @@ namespace App\Filament\Resources\PcaResource\Pages;
 use App\Filament\Resources\PcaResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
+
 
 class CreatePca extends CreateRecord
 {
     protected static string $resource = PcaResource::class;
+    protected static ?string $title = "Buat Data PCA";
+    protected function getCreateFormAction(): Actions\Action
+    {
+        return parent::getCreateFormAction()
+        ->label('Tambah Data');
+    }
+
+    protected function getCreateAnotherFormAction(): Actions\Action
+    {
+        return parent::getCreateAnotherFormAction()
+        ->label('Tambah Data Lagi')
+        ->color('warning');
+    }
+    
+    protected function getCancelFormAction() : Actions\Action
+    {
+        return parent::getCancelFormAction()
+        ->label('Batal')
+        ->color('danger');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Data PCA Disimpan')
+            ->body('Data PCA telah berhasil disimpan.');
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
 }
+
+
--- app/Filament/Resources/PcaResource/Pages/ListPcas.php
+++ app/Filament/Resources/PcaResource/Pages/ListPcas.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\PcaResource\Pages;
 use App\Filament\Resources\PcaResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\PcaResource\Widgets\pcaStats;
+
 
 class ListPcas extends ListRecords
 {
@@ -13,7 +15,20 @@ class ListPcas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Data PCA'),
         ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            pcaStats::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
