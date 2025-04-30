ab3078b7e1ffa83f89f3740756d2b06e47228675
hanaageofanii <hanaageofanii@gmail.com>
2025-04-30 10:14:20 +0700

    modified pages edit list create in form legal pca

--- app/Filament/Resources/FormLegalPcaResource/Pages/ListFormLegalPcas.php
+++ app/Filament/Resources/FormLegalPcaResource/Pages/ListFormLegalPcas.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\FormLegalPcaResource\Pages;
 use App\Filament\Resources\FormLegalPcaResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\FormLegalPcaResource\Widgets\form_legal_pca;
+
 
 class ListFormLegalPcas extends ListRecords
 {
@@ -13,7 +15,20 @@ class ListFormLegalPcas extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Data Sertifikat'),
         ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            form_legal_pca::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
