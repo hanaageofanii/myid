2a519363ba59a60741341c84b16119fc3d3dff2a
hanaageofanii <hanaageofanii@gmail.com>
2025-05-02 11:57:11 +0700

    modified page

--- app/Filament/Resources/LegalTkrResource/Pages/EditLegalTkr.php
+++ app/Filament/Resources/LegalTkrResource/Pages/EditLegalTkr.php
@@ -10,10 +10,22 @@ class EditLegalTkr extends EditRecord
 {
     protected static string $resource = LegalTkrResource::class;
 
+    protected static ?string $title = "Ubah Data Sertifikat";
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+            ->label('Hapus Data Sertifikat')
+            ->modalHeading(fn ($record) => "Konfirmasi Hapus {$record->siteplan}")
+            ->modalDescription(fn ($record) => "Apakah Anda yakin ingin menghapus blok {$record->siteplan}?"),
         ];
     }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
+    
 }
--- app/Filament/Resources/LegalTkrResource/Pages/ListLegalTkrs.php
+++ app/Filament/Resources/LegalTkrResource/Pages/ListLegalTkrs.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\LegalTkrResource\Pages;
 use App\Filament\Resources\LegalTkrResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\LegalTkrResource\Widgets\LegalTkr;
+
 
 class ListLegalTkrs extends ListRecords
 {
@@ -13,7 +15,20 @@ class ListLegalTkrs extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Data Sertifikat'),
         ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            LegalTkr::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
