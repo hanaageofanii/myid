9804c774c22d41e87eac3ca20cc73cb88ffa7552
hanaageofanii <hanaageofanii@gmail.com>
2025-05-02 12:11:20 +0700

    modified page pajak tkr

--- app/Filament/Resources/PajakTkrResource/Pages/ListPajakTkrs.php
+++ app/Filament/Resources/PajakTkrResource/Pages/ListPajakTkrs.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\PajakTkrResource\Pages;
 use App\Filament\Resources\PajakTkrResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\PajakTkrResource\Widgets\PajakTkr;
+
 
 class ListPajakTkrs extends ListRecords
 {
@@ -13,7 +15,20 @@ class ListPajakTkrs extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Data Validasi PPH'),
         ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            PajakTkr::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
