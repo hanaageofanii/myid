638f4c7e3d9a9a215ad1841f2a5cfbeb5298d844
hanaageofanii <hanaageofanii@gmail.com>
2025-06-20 09:37:36 +0700

    rename FormDPTkr

--- app/Policies/FormDpTkrPolicy.php
+++ app/Policies/FormDpTkrPolicy.php
@@ -3,8 +3,9 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\FormDpTkr;
 
-class FormDpTkr
+class FormDpTkrPolicy
 {
     /**
      * Determine whether the user can view any models.
