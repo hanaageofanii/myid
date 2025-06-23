ccb241ea5dd88917641a8dcfa31ba1becd156ee3
hanaageofanii <hanaageofanii@gmail.com>
2025-06-23 15:59:15 +0700

    updated database gcv tanda terima

--- database/migrations/2025_06_23_081037_create_gcv_datatandaterimas_table.php
+++ database/migrations/2025_06_23_081037_create_gcv_datatandaterimas_table.php
@@ -13,6 +13,35 @@ return new class extends Migration
     {
         Schema::create('gcv_datatandaterimas', function (Blueprint $table) {
             $table->id();
+
+            $table->string('siteplan');
+            $table->integer('luas');
+            $table->string('type');
+            $table->boolean('terbangun')->default(false);
+            $table->enum('status', ['akad','stock'])->nullable();
+            $table->enum('status_bn', ['sudah','belum'])->nullable();
+
+            $table->string('kode1')->nullable();
+            $table->integer('luas1')->nullable();
+            $table->string('kode2')->nullable();
+            $table->integer('luas2')->nullable();
+            $table->string('kode3')->nullable();
+            $table->integer('luas3')->nullable();
+            $table->string('kode4')->nullable();
+            $table->integer('luas4')->nullable();
+
+            $table->string('tanda_terima_sertifikat')->nullable();
+            $table->string('nop_pbb_pecahan')->nullable();
+            $table->string('tanda_terima_nop')->nullable();
+            $table->string('imb_pbg')->nullable();
+            $table->string('tanda_terima_imb_pbg')->nullable();
+            $table->text('tanda_terima_tambahan')->nullable();
+
+            $table->json('up_sertifikat')->nullable();
+            $table->json('up_nop')->nullable();
+            $table->json('up_imb_pbg')->nullable();
+            $table->json('up_tambahan_lainnya')->nullable();
+
             $table->timestamps();
         });
     }
