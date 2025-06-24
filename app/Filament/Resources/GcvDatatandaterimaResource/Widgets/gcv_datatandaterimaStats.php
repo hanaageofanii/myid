db810f2e6e5ea8d652713d35aca0c66c741e2602
hanaageofanii <hanaageofanii@gmail.com>
2025-06-24 11:13:49 +0700

    update stats

--- app/Filament/Resources/GcvDatatandaterimaResource/Widgets/gcv_datatandaterimaStats.php
+++ app/Filament/Resources/GcvDatatandaterimaResource/Widgets/gcv_datatandaterimaStats.php
@@ -12,49 +12,30 @@ class gcv_datatandaterimaStats extends BaseWidget
     protected static ?string $maxHeight = '300px';
     protected static ?string $pollingInterval = null;
     protected static bool $isLazy = false;
-    protected int | string | array $columns = 5;
     protected ?string $heading = 'Dashboard Statistik GCV Data Tanda Terima';
+
+
     protected function getStats(): array
     {
-        $total = gcv_datatandaterima::count();
-        $totalAkad = gcv_datatandaterima::where('status', 'akad')->count();
-        $bnSudah = gcv_datatandaterima::where('status_bn', 'sudah')->count();
-        $bnBelum = gcv_datatandaterima::where('status_bn', 'belum')->count();
-        $kavlingStandar = gcv_datatandaterima::where('kavling', 'standar')->count();
-        $kavlingKomersil = gcv_datatandaterima::where('kavling', 'komersil')->count();
-        $kavlingHook = gcv_datatandaterima::where('kavling', 'hook')->count();
-        $kavlingTanahLebih = gcv_datatandaterima::where('kavling', 'tanah_lebih')->count();
-        $kavlingKhusus = gcv_datatandaterima::where('kavling', 'khusus')->count();
-        $kavlingKios = gcv_datatandaterima::where('kavling', 'kios')->count();
         return [
-            Stat::make('Total Data Tanda Terima', $total)
-                ->description('Semua data kavling yang terdaftar'),
-            Stat::make('Status Akad', $totalAkad)
-                ->description('Jumlah kavling yang sudah akad')
+            Stat::make('Total Data', gcv_datatandaterima::count()),
+            Stat::make('Status Akad', gcv_datatandaterima::where('status', 'akad')->count())
                 ->color('success'),
-            Stat::make('BN Sudah Selesai', $bnSudah)
-                ->description('Penyelesaian BN selesai')
+            Stat::make('Status Sudah BN', gcv_datatandaterima::where('status_bn', 'sudah')->count())
                 ->color('primary'),
-            Stat::make('BN Belum Selesai', $bnBelum)
-                ->description('Penyelesaian BN belum selesai')
+            Stat::make('Status Belum BN', gcv_datatandaterima::where('status_bn', 'belum')->count())
                 ->color('danger'),
-            Stat::make('Kavling Standar', $kavlingStandar)
-                ->description('Jumlah kavling jenis Standar')
+            Stat::make('Kavling Standar', gcv_datatandaterima::where('kavling', 'standar')->count())
                 ->color('info'),
-            Stat::make('Kavling Komersil', $kavlingKomersil)
-                ->description('Jumlah kavling jenis Komersil')
+            Stat::make('Kavling Komersil', gcv_datatandaterima::where('kavling', 'komersil')->count())
                 ->color('info'),
-            Stat::make('Kavling Hook', $kavlingHook)
-                ->description('Jumlah kavling jenis Hook')
+            Stat::make('Kavling Hook', gcv_datatandaterima::where('kavling', 'hook')->count())
                 ->color('info'),
-            Stat::make('Kavling Tanah Lebih', $kavlingTanahLebih)
-                ->description('Jumlah kavling jenis Tanah Lebih')
+            Stat::make('Kavling Tanah Lebih', gcv_datatandaterima::where('kavling', 'tanah_lebih')->count())
                 ->color('info'),
-            Stat::make('Kavling Khusus', $kavlingKhusus)
-                ->description('Jumlah kavling jenis Khusus')
+            Stat::make('Kavling Khusus', gcv_datatandaterima::where('kavling', 'khusus')->count())
                 ->color('info'),
-            Stat::make('Kavling Kios', $kavlingKios)
-                ->description('Jumlah kavling jenis Kios')
+            Stat::make('Kavling Kios', gcv_datatandaterima::where('kavling', 'kios')->count())
                 ->color('info'),
         ];
     }
