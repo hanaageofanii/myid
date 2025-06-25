b920badd0fde755d83c79d5e2b8d2672e4a9e4fa
hanaageofanii <hanaageofanii@gmail.com>
2025-06-25 13:51:45 +0700

    update widget stok gcv

--- app/Filament/Resources/GcvStokResource/Widgets/gcv_stokStats.php
+++ app/Filament/Resources/GcvStokResource/Widgets/gcv_stokStats.php
@@ -4,13 +4,31 @@ namespace App\Filament\Resources\GcvStokResource\Widgets;
 
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\gcv_stok;
+use Filament\Widgets\StatsOverviewWidget\Card;
 
 class gcv_stokStats extends BaseWidget
 {
+    protected static ?string $maxHeight = '300px';
+    protected static ?string $pollingInterval = '10s';
+    protected static bool $isLazy = false;
+    protected ?string $heading = 'Dashboard Stok';
     protected function getStats(): array
     {
         return [
-            //
+            Card::make('Total Data Tkr', gcv_stok::count())
+            ->extraAttributes([
+                'style' => 'background-color: #ffff; border-color: #234C63;'
+            ]),
+            Card::make('Total Tkr', gcv_stok::where('proyek', 'tkr')->count())
+            ->color('white')
+            ->extraAttributes([
+                'style' => 'background-color: #ffff; border-color: #234C63; color: #234C63; font-weight: bold;'
+            ]),
+            Card::make('Total Tkr Cira', gcv_stok::where('proyek', 'tkr_cira')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
         ];
     }
 }
