8f474305f458599f8b49dd7d0ed82f305a8785bd
hanaageofanii <hanaageofanii@gmail.com>
2025-06-26 14:57:28 +0700

    update model pencairan dajam

--- app/Models/gcv_pencairan_dajam.php
+++ app/Models/gcv_pencairan_dajam.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class gcv_pencairan_dajam extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, HasRoles;
+
+    protected $fillable = [
+            'siteplan',
+            'bank',
+            'no_debitur',
+            'nama_konsumen',
+            'nama_dajam',
+            'nilai_dajam',
+            'tanggal_pencairan',
+            'nilai_pencairan',
+            'selisih_dajam',
+            'up_rekening_koran',
+            'up_lainnya',
+    ];
+
+    protected $casts =[
+        "up_rekening_korang" => 'array',
+        "up_lainnya" => 'array'
+    ];
 }
