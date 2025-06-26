7d242018c9800eb9df48b4df14c3be318b2c5428
hanaageofanii <hanaageofanii@gmail.com>
2025-06-26 12:04:30 +0700

    update migration pencairan akad & model

--- database/migrations/2025_06_26_035400_create_gcv_pencairan_akads_table.php
+++ database/migrations/2025_06_26_035400_create_gcv_pencairan_akads_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('gcv_pencairan_akads', function (Blueprint $table) {
             $table->id();
+            $table->string('siteplan')->nullable();
+            $table->enum('bank',['btn_cikarang','btn_bekasi','btn_karawang','bjb_syariah','bjb_jababeka','btn_syariah','brii_bekasi'])->nullable();
+            $table->string('nama_konsumen')->nullable();
+            $table->string('max_kpr')->nullable();
+            $table->date('tanggal_pencairan')->nullable();
+            $table->string('nilai_pencairan')->nullable();
+            $table->string('dana_jaminan')->nullable();
+            $table->string('no_debitur')->nullable();
+            $table->string('jenis_pembayaran')->nullable();
+            $table->enum('kavling',['standar','khusus','hook','komersil','tanah_lebih','kios'])->nullable();
+            $table->json('up_spd5')->nullable();
+            $table->json('up_rekening_koran')->nullable();
+
             $table->timestamps();
         });
     }
