dc9265b937c4e134c9d532488bbbbfd6723b1dce
hanaageofanii <hanaageofanii@gmail.com>
2025-06-26 13:20:49 +0700

    update pencairan akad policy

--- app/Policies/gcv_pencairan_akadPolicy.php
+++ app/Policies/gcv_pencairan_akadPolicy.php
@@ -3,14 +3,62 @@
 namespace App\Policies;
 
 use App\Models\User;
+use App\Models\gcv_pencairan_akad;
 
 class gcv_pencairan_akadPolicy
-{
-    /**
-     * Create a new policy instance.
+{/**
+     * Determine whether the user can view any models.
      */
-    public function __construct()
+    public function viewAny(User $user): bool
     {
-        //
+        return $user->hasRole(['admin','Direksi','Kasir 1','Kasir 2']);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, gcv_pencairan_akad $gcv_pencairan_akad): bool
+    {
+        return $user->hasRole(['admin','Direksi','Kasir 1','Kasir 2']);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return $user->hasRole(['admin','Kasir 1','Kasir 2']);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, gcv_pencairan_akad $gcv_pencairan_akad): bool
+    {
+        return $user->hasRole(['admin','Kasir 1','Kasir 2']);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, gcv_pencairan_akad $gcv_pencairan_akad): bool
+    {
+        return $user->hasRole(['admin','Kasir 1','Kasir 2']);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, gcv_pencairan_akad $gcv_pencairan_akad): bool
+    {
+        return $user->hasRole(['admin','Kasir 1','Kasir 2']);
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, gcv_pencairan_akad $gcv_pencairan_akad): bool
+    {
+        return $user->hasRole(['admin','Kasir 1','Kasir 2']);
     }
 }
