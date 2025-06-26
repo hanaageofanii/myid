01c8f48861f16c7a337dbde8f2a7774ecbd269b3
hanaageofanii <hanaageofanii@gmail.com>
2025-06-26 10:43:59 +0700

    update page gcv kpr

--- app/Filament/Resources/GcvKprResource/Pages/CreateGcvKpr.php
+++ app/Filament/Resources/GcvKprResource/Pages/CreateGcvKpr.php
@@ -3,10 +3,44 @@
 namespace App\Filament\Resources\GcvKprResource\Pages;
 
 use App\Filament\Resources\GcvKprResource;
+use Filament\Notifications\Notification;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateGcvKpr extends CreateRecord
 {
-    protected static string $resource = GcvKprResource::class;
+     protected static ?string $title = "Buat Data KPR";
+    protected function getCreateFormAction(): Actions\Action
+    {
+        return parent::getCreateFormAction()
+        ->label('Tambah Data KPR');
+    }
+
+    protected function getCreateAnotherFormAction(): Actions\Action
+    {
+        return parent::getCreateAnotherFormAction()
+        ->label('Tambah Data Lagi')
+        ->color('warning');
+    }
+
+    protected function getCancelFormAction() : Actions\Action
+    {
+        return parent::getCancelFormAction()
+        ->label('Batal')
+        ->color('danger');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Data KPR Disimpan')
+            ->body('Data KPR telah berhasil disimpan.');
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
+    }
 }
--- app/Filament/Resources/GcvKprResource/Pages/ListGcvKprs.php
+++ app/Filament/Resources/GcvKprResource/Pages/ListGcvKprs.php
@@ -5,15 +5,31 @@ namespace App\Filament\Resources\GcvKprResource\Pages;
 use App\Filament\Resources\GcvKprResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\GcvKprResource\Widgets\gcv_kprStats;
+
 
 class ListGcvKprs extends ListRecords
 {
     protected static string $resource = GcvKprResource::class;
 
-    protected function getHeaderActions(): array
+   protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Data KPR'),
         ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            gcv_kprStats::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
