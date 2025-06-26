4817992cc815e23a09919f9d56a12475f4b28f62
hanaageofanii <hanaageofanii@gmail.com>
2025-06-26 13:19:15 +0700

    update pencairan akad stats

--- app/Filament/Resources/GcvPencairanAkadResource/Widgets/gcv_pencairan_akadStats.php
+++ app/Filament/Resources/GcvPencairanAkadResource/Widgets/gcv_pencairan_akadStats.php
@@ -3,14 +3,59 @@
 namespace App\Filament\Resources\GcvPencairanAkadResource\Widgets;
 
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use App\Models\gcv_pencairan_akad;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class gcv_pencairan_akadStats extends BaseWidget
 {
+    protected static ?string $maxHeight = '300px';
+    protected static ?string $pollingInterval = '10s';
+    protected static bool $isLazy = false;
+    protected ?string $heading = 'Dashboard Pencairan Akad PCA';
     protected function getStats(): array
     {
         return [
-            //
+            Card::make('Total Data Pencairan Akad',  gcv_pencairan_akad::count())
+            ->extraAttributes([
+                'style' => 'background-color: #ffff; border-color: #234C63;'
+            ]),
+
+            Card::make('BTN Cikarang',  gcv_pencairan_akad::where('bank', 'BTN Cikarang')->count())
+            ->extraAttributes([
+                'style' => 'background-color: #ffff; border-color: #234C63;'
+            ]),
+
+            Card::make('BTN Bekasi',  gcv_pencairan_akad::where('bank', 'btn_bekasi')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BTN Karawang',  gcv_pencairan_akad::where('bank', 'btn_karawang')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BJB Syariah',  gcv_pencairan_akad::where('bank', 'bjb_syariah')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BJB Jababeka',  gcv_pencairan_akad::where('bank', 'bjb_jababeka')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BTN Syariah',  gcv_pencairan_akad::where('bank', 'btn_syariah')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BRI Bekasi',  gcv_pencairan_akad::where('bank', 'brii_bekasi')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
         ];
     }
+
 }
