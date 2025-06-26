407b0c3d27282b631f04eb822ddf8da3519f006e
hanaageofanii <hanaageofanii@gmail.com>
2025-06-26 15:10:15 +0700

    update yuhuuw

--- app/Filament/Resources/GcvPencairanDajamResource.php
+++ app/Filament/Resources/GcvPencairanDajamResource.php
@@ -17,7 +17,13 @@ class GcvPencairanDajamResource extends Resource
 {
     protected static ?string $model = gcv_pencairan_dajam::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $title = "Data Pencairan Dajam";
+    protected static ?string $navigationGroup = "GCV";
+    protected static ?string $pluralLabel = "Data Pencairan Dajam";
+    protected static ?string $navigationLabel = 'Keuangan > Pencairan Dajam';
+    protected static ?string $pluralModelLabel = 'Data Pencairan Dajam';
+    protected static ?int $navigationSort = 7;
+    protected static ?string $navigationIcon = 'heroicon-o-queue-list';
 
     public static function form(Form $form): Form
     {
