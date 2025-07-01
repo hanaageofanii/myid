13f9285ff7889c6ffaff3c2d60e00cd90b1390e3
hanaageofanii <hanaageofanii@gmail.com>
2025-07-01 09:30:51 +0700

    add model & database + column deleted

---
+++ database/migrations/2025_07_01_021802_create_gcv_uang_mukas_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gcv_uang_mukas', function (Blueprint $table) {
+            $table->id();
+
+            $table->enum('kavling',['standar','khusus','hook','komersil','tanah_lebih','kios'])->nullable();
+            $table->string('siteplan')->nullable();
+            $table->string('nama_konsumen')->nullable();
+            $table->string('harga')->nullable();
+            $table->string('max_kpr')->nullable();
+
+            $table->string('sbum')->nullable();
+            $table->string('sisa_pembayaran')->nullable();
+            $table->string('dp')->nullable();
+            $table->string('laba_rugi')->nullable();
+            $table->date('tanggal_terima_dp')->nullable();
+            $table->enum('pembayaran', ['cash','potong_komisi','promo'])->nullable();
+
+            $table->json('up_kwitansi')->nullable();
+            $table->json('up_pricelist')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gcv_uang_mukas');
+    }
+};
