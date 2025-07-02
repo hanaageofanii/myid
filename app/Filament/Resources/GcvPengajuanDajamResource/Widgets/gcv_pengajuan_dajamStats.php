9c7a12e876e9bcc653389c8e11df22974d31ebf2
hanaageofanii <hanaageofanii@gmail.com>
2025-07-02 15:48:54 +0700

    update widget

--- app/Filament/Resources/GcvPengajuanDajamResource/Widgets/gcv_pengajuan_dajamStats.php
+++ app/Filament/Resources/GcvPengajuanDajamResource/Widgets/gcv_pengajuan_dajamStats.php
@@ -2,15 +2,101 @@
 
 namespace App\Filament\Resources\GcvPengajuanDajamResource\Widgets;
 
+use App\Models\gcv_pengajuan_dajam;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget\Card;
+
 
 class gcv_pengajuan_dajamStats extends BaseWidget
 {
+    protected static ?int $sort = 12;
+
+    protected static ?string $maxHeight = '300px';
+    protected static ?string $pollingInterval = '10s';
+    protected static bool $isLazy = false;
+    protected ?string $heading = 'Dashboard Pangajuan Dajam GCV';
     protected function getStats(): array
     {
         return [
-            //
+            Card::make('Total Pengajuan Dajam', gcv_pengajuan_dajam::count())
+            ->extraAttributes([
+                'style' => 'background-color: #ffff; border-color: #234C63;'
+            ]),
+            Card::make('Total Site Plan', gcv_pengajuan_dajam::distinct('siteplan')->count('siteplan'))
+            ->extraAttributes([
+                'style' => 'background-color: #ffff; border-color: #234C63;'
+            ]),
+            Card::make('BTN Cikarang', gcv_pengajuan_dajam::where('bank', 'BTN Cikarang')->count())
+            ->extraAttributes([
+                'style' => 'background-color: #ffff; border-color: #234C63;'
+            ]),
+
+            Card::make('BTN Bekasi', gcv_pengajuan_dajam::where('bank', 'btn_bekasi')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BTN Karawang', gcv_pengajuan_dajam::where('bank', 'btn_karawang')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BJB Syariah', gcv_pengajuan_dajam::where('bank', 'bjb_syariah')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BJB Jababeka', gcv_pengajuan_dajam::where('bank', 'bjb_jababeka')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BTN Syariah', gcv_pengajuan_dajam::where('bank', 'btn_syariah')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+
+            Card::make('BRI Bekasi', gcv_pengajuan_dajam::where('bank', 'brii_bekasi')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+            Card::make('Sudah Diajukan', gcv_pengajuan_dajam::where('status_dajam', 'sudah_diajukan')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+            Card::make('Belum Diajukan', gcv_pengajuan_dajam::where('status_dajam', 'belum_diajukan')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+            Card::make('Dajam Sertifikat', gcv_pengajuan_dajam::where('nama_dajam', 'sertifikat')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+            Card::make('Dajam IMB', gcv_pengajuan_dajam::where('nama_dajam', 'imb')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+            Card::make('Dajam Listrik', gcv_pengajuan_dajam::where('nama_dajam', 'listrik')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+                Card::make('Dajam Bestek', gcv_pengajuan_dajam::where('nama_dajam', 'bestek')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+            Card::make('Dajam JKK', gcv_pengajuan_dajam::where('nama_dajam', 'jkk')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+            Card::make('Dajam PPH', gcv_pengajuan_dajam::where('nama_dajam', 'pph')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
+            Card::make('Dajam BPHTB', gcv_pengajuan_dajam::where('nama_dajam', 'bphtb')->count())
+                ->extraAttributes([
+                    'style' => 'background-color: #ffff; border-color: #234C63;'
+                ]),
         ];
     }
 }
