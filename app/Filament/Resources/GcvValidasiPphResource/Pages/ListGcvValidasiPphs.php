dfe4d2a30aa4fada8b16b8d00706a47683bc69da
hanaageofanii <hanaageofanii@gmail.com>
2025-07-02 10:20:02 +0700

    update page gcv validasi pph

--- app/Filament/Resources/GcvValidasiPphResource/Pages/ListGcvValidasiPphs.php
+++ app/Filament/Resources/GcvValidasiPphResource/Pages/ListGcvValidasiPphs.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\GcvValidasiPphResource\Pages;
 use App\Filament\Resources\GcvValidasiPphResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\GcvValidasiPphResource\Widgets\gcv_validasi_pphStats;
+
 
 class ListGcvValidasiPphs extends ListRecords
 {
@@ -13,7 +15,20 @@ class ListGcvValidasiPphs extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Data Validasi PPH'),
         ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            gcv_validasi_pphStats::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
