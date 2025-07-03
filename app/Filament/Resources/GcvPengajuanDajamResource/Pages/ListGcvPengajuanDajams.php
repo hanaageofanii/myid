88e5cbd6917e7b79921243491da4654656259aff
hanaageofanii <hanaageofanii@gmail.com>
2025-07-03 10:37:19 +0700

    update pages gcv pengajuan dajjam

--- app/Filament/Resources/GcvPengajuanDajamResource/Pages/ListGcvPengajuanDajams.php
+++ app/Filament/Resources/GcvPengajuanDajamResource/Pages/ListGcvPengajuanDajams.php
@@ -5,15 +5,30 @@ namespace App\Filament\Resources\GcvPengajuanDajamResource\Pages;
 use App\Filament\Resources\GcvPengajuanDajamResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\GcvPengajuanDajamResource\Widgets\gcv_pengajuan_dajamStats;
+
 
 class ListGcvPengajuanDajams extends ListRecords
 {
     protected static string $resource = GcvPengajuanDajamResource::class;
 
-    protected function getHeaderActions(): array
+   protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Buat Pengajuan Dajam'),
         ];
+    }
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            gcv_pengajuan_dajamStats::class,
+        ];
+    }
+
+    protected function getSaveFormAction(): Actions\Action
+    {
+        return parent::getSaveFormAction()
+        ->label('Simpan');
     }
 }
