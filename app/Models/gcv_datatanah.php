51c15b81b72128c2499eabf31bdc5dba24d09791
hanaageofanii <hanaageofanii@gmail.com>
2025-07-04 10:56:50 +0700

    update model data tanah

--- app/Models/gcv_datatanah.php
+++ app/Models/gcv_datatanah.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Permission\Traits\HasRoles;
 
 class gcv_datatanah extends Model
 {
-    use HasFactory;
+    use HasFactory, HasRoles, SoftDeletes;
+        protected $filliable = [
+            "no_bidang",
+            "nama_pemilik_asal",
+            "alas_hak",
+            "luas_surat",
+            "luas_ukur",
+            "nop",
+            "harga_jual",
+            "sph",
+            "notaris",
+            "catatan"
+        ];
+
+    protected $casts = [
+        "up_sertifikat" => 'array',
+        "up_nop" => 'array',
+        "up_sph" => 'array',
+        "up_tambahan_lainnya" => 'array',
+    ];
 }
