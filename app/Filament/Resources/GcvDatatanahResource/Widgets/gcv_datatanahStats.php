003a27e0d73e4e4ef86b8b3c99d7bbe948288269
hanaageofanii <hanaageofanii@gmail.com>
2025-07-04 10:52:48 +0700

    update stats

--- app/Filament/Resources/GcvDatatanahResource/Widgets/gcv_datatanahStats.php
+++ app/Filament/Resources/GcvDatatanahResource/Widgets/gcv_datatanahStats.php
@@ -4,13 +4,19 @@ namespace App\Filament\Resources\GcvDatatanahResource\Widgets;
 
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use App\Models\gcv_datatanah;
 
 class gcv_datatanahStats extends BaseWidget
 {
+    protected static ?string $maxHeight = '300px';
+    protected static ?string $pollingInterval = null;
+    protected static bool $isLazy = false;
+    protected ?string $heading = 'Dashboard Statistik Data Tanah';
+
     protected function getStats(): array
     {
         return [
-            //
+            Stat::make('Total Data Tanah', gcv_datatanah::count()),
         ];
     }
 }
