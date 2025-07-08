760fa08396bbe3d3a47b0ba8e0e782e8242539de
hanaageofanii <hanaageofanii@gmail.com>
2025-07-08 14:31:54 +0700

    add pritected & label

--- app/Filament/Resources/GcvPengajuanBnResource.php
+++ app/Filament/Resources/GcvPengajuanBnResource.php
@@ -13,11 +13,57 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+use App\Models\gcv_pencairan_akad;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
+use App\Models\gcv_stok;
+use App\Models\gcv_kpr;
+use App\Models\gcvDataSiteplan;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Fieldset;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Enums\ActionsPosition;
+use Illuminate\Database\Eloquent\Collection;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Tables\Actions\Action;
+use Filament\Notifications\Notification;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+use App\Models\gcv_datatandaterima;
+use Filament\Forms\Components\Repeater;
+use App\Filament\Resources\GcvLegalitasResource\Widgets\gcv_legalitasStats;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Actions\ForceDeleteAction;
+use Carbon\Carbon;
+
 class GcvPengajuanBnResource extends Resource
 {
     protected static ?string $model = gcv_pengajuan_bn::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $title = "Data Pengajuan BN";
+    protected static ?string $navigationGroup = "GCV";
+    protected static ?string $pluralLabel = "Data Pengajuan BN";
+    protected static ?string $navigationLabel = 'Legal > Pengajuan BN';
+    protected static ?string $pluralModelLabel = 'Data Pengajuan BN';
+    protected static ?int $navigationSort = 17;
+    protected static ?string $navigationIcon = 'heroicon-o-queue-list';
 
     public static function form(Form $form): Form
     {
