72080e58e4a30e75c50fce1a8fba630a2556d4e3
hanaageofanii <hanaageofanii@gmail.com>
2025-07-25 10:22:03 +0700

    update pages list empty state icon

--- app/Filament/Resources/GcvUangMukaResource/Pages/ListGcvUangMukas.php
+++ app/Filament/Resources/GcvUangMukaResource/Pages/ListGcvUangMukas.php
@@ -6,6 +6,8 @@ use App\Filament\Resources\GcvUangMukaResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use App\Filament\Resources\GcvUangMukaResource\Widgets\gcv_uangMukaStats;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Table;
 
 
 class ListGcvUangMukas extends ListRecords
@@ -31,4 +33,20 @@ class ListGcvUangMukas extends ListRecords
         return parent::getSaveFormAction()
         ->label('Simpan');
     }
+
+    public function getTable(): Table
+        {
+            return parent::getTable()
+                ->emptyStateIcon('heroicon-o-bookmark')
+                ->emptyStateDescription('Silakan buat data pengajuan BN')
+                ->emptyStateHeading('Belum ada data pengajuan BN')
+                ->emptyStateActions([
+                    Action::make('create')
+                        ->label('Buat Data Pengajuan BN')
+                        ->url($this->getResource()::getUrl('create'))
+                        ->icon('heroicon-m-plus')
+                        ->button(),
+                ]);
+        }
+
 }
