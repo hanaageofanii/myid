dedfa07f3ee2b4f1e30261a3ca234b503e97fa98
hanaageofanii <hanaageofanii@gmail.com>
2025-08-04 10:18:35 +0700

    update migration deleted table

--- database/migrations/2025_08_04_031440_add_column_deleted_at_to_kartu_kontrol_g_c_v_s.php
+++ database/migrations/2025_08_04_031440_add_column_deleted_at_to_kartu_kontrol_g_c_v_s.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('kartu_kontrol_g_c_v_s', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
