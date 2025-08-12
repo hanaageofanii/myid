4b87ba9e264e8cbae21f4b383ba866a26d25caff
hanaageofanii <hanaageofanii@gmail.com>
2025-08-12 09:45:32 +0700

    add database kartu kontrol

--- database/migrations/2025_08_04_031148_create_kartu_kontrol_g_c_v_s_table.php
+++ database/migrations/2025_08_04_031148_create_kartu_kontrol_g_c_v_s_table.php
@@ -13,6 +13,42 @@ return new class extends Migration
     {
         Schema::create('kartu_kontrol_g_c_v_s', function (Blueprint $table) {
             $table->id();
+             $table->enum('proyek', ['gcv_cira','gcv','tkr','tkr_cira','pca1'])->nullable();
+            $table->string('lokasi_proyek')->nullable( );
+            $table->string('nama_konsumen')->nullable( );
+            $table->enum('nama_perusahaan', ['langgeng_pertiwi_development','agung_purnama_bakti','purnama_karya_bersama'])->nullable();
+            $table->string('alamat')->nullable( );
+            $table->string('no_telepon')->nullable( );
+            $table->enum('kavling', ['standar','khusus','hook','komersil','tanah_lebih','kios'])->nullable();
+            $table->string('siteplan')->nullable( );
+            $table->string('type')->nullable( );
+            $table->string('luas')->nullable( );
+            $table->date('tanggal_booking')->nullable( );
+            $table->string('agent')->nullable( );
+            $table->string('bank')->nullable( );
+            $table->string('notaris')->nullable( );
+            $table->date('tanggal_akad')->nullable( );
+            $table->string('harga_jual')->nullable( );
+            $table->string('harga/m')->nullable( );
+            $table->string('pajak')->nullable( );
+            $table->string('biaya_proses')->nullable( );
+            $table->string('uang_muka')->nullable( );
+            $table->string('estimasi_kpr')->nullable( );
+            $table->string('realisasi_kpr')->nullable( );
+            $table->string('selisih_kpr')->nullable( );
+            $table->string('sbum&disct')->nullable( );
+            $table->string('biaya_lain')->nullable( );
+            $table->string('total_biaya')->nullable( );
+            $table->string('no_konsumen')->nullable( );
+            $table->date('tanggal_pembayaran')->nullable( );
+            $table->string('keterangan')->nullable( );
+            $table->string('nilai_kontrak')->nullable( );
+            $table->string('pembayaran')->nullable( );
+            $table->string('sisa/saldo')->nullable( );
+            $table->string('paraf')->nullable( );
+            $table->string('catatan')->nullable( );
+            $table->json('bukti_lainnya')->nullable( );
+            $table->string('status')->nullable( );
             $table->timestamps();
         });
     }
