8896c0afa902f9d739b86f197e26168dc58ed328
hanaageofanii <hanaageofanii@gmail.com>
2025-10-02 16:39:44 +0700

    update kartu control

--- app/Filament/Resources/KartuKontrolGCVResource.php
+++ app/Filament/Resources/KartuKontrolGCVResource.php
@@ -22,6 +22,43 @@ use App\Models\gcv_kpr;
 use Filament\Forms\Components\FileUpload;
 use Closure;
 
+use App\Filament\Resources\GcvUangMukaResource\RelationManagers;
+use App\Models\gcv_uang_muka;
+use App\Models\GcvUangMuka;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Models\gcv_stok;
+use App\Models\gcvDataSiteplan;
+use App\Models\gcv_legalitas;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Fieldset;
+use Filament\Tables\Columns\BooleanColumn;
+use Filament\Tables\Actions\BulkAction;
+use Filament\Tables\Enums\ActionsPosition;
+use Illuminate\Database\Eloquent\Collection;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
+use Filament\Support\Enums\MaxWidth;
+use Filament\Tables\Actions\Action;
+use Filament\Notifications\Notification;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Actions\ForceDeleteAction;
+use App\Models\form_kpr;
+use Filament\Forms\Components\Wizard\Step;
+use Illuminate\Validation\Rule;
+use App\Models\gcv_datatandaterima;
+use Carbon\Carbon;
+use App\Models\gcv_pencairan_akad;
+use Filament\Forms\Components\Repeater;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use App\Filament\Resources\GcvUangMukaResource\Widgets\gcv_uang_MukaStats;
+
+
 
 class KartuKontrolGCVResource extends Resource
 {
@@ -274,7 +311,7 @@ class KartuKontrolGCVResource extends Resource
                                                 return $user && $user->hasRole(['admin', 'Kasir 1', 'Kasir 2']);
                                             })()),
                                         TextInput::make('uang_muka')
-                                            ->label('Uang Muka')
+                                            ->label('Kartu Kontrol')
                                             ->numeric()
                                             ->reactive()
                                             ->prefix('Rp')
@@ -535,25 +572,211 @@ class KartuKontrolGCVResource extends Resource
                 ->html()
                 ->sortable(),
             ])
+                        ->defaultSort('siteplan', 'asc')
+            ->headerActions([
+                Action::make('count')
+                    ->label(fn ($livewire): string => 'Total: ' . $livewire->getFilteredTableQuery()->count())
+                    ->disabled(),
+            ])
             ->filters([
-                // Tambahkan filter jika perlu
-            ])
+                TrashedFilter::make()
+                ->label('Data yang dihapus')
+                ->native(false),
+
+                Filter::make('kavling')
+                    ->label('Jenis Unit')
+                    ->form([
+                        Select::make('kavling')
+                            ->options([
+                                'standar' => 'Standar',
+                                'khusus' => 'Khusus',
+                                'hook' => 'Hook',
+                                'komersil' => 'Komersil',
+                                'tanah_lebih' => 'Tanah Lebih',
+                                'kios' => 'Kios',
+                            ])
+                            ->nullable()
+                            ->native(false),
+                    ])
+                    ->query(fn ($query, $data) =>
+                        $query->when(isset($data['kavling']), fn ($q) =>
+                            $q->where('kavling', $data['kavling'])
+                        )
+                    ),
+
+                    Filter::make('nama_proyek')
+                    ->label('Nama Proyek')
+                    ->form([
+                        Select::make('nama_proyek')
+                            ->options([
+                                        'gcv_cira' => 'GCV Cira',
+                                        'gcv' => 'GCV',
+                                        'tkr' => 'TKR',
+                                        'tkr_cira' => 'TKR Cira',
+                                        'pca1' => 'PCA1',
+
+                            ])
+                            ->nullable()
+                            ->native(false),
+                    ])
+                    ->query(fn ($query, $data) =>
+                        $query->when(isset($data['nama_proyek']), fn ($q) =>
+                            $q->where('nama_proyek', $data['nama_proyek'])
+                        )
+                    ),
+
+                    Filter::make('nama_perusahaan')
+                    ->label('Nama Perusahaan')
+                    ->form([
+                        Select::make('nama_perusahaan')
+                            ->options([
+                                'langgeng_pertiwi_development' => 'PT. Langgeng Pertiwi Development',
+                                'agung_purnama_bakti' => 'PT. Agung Purnama Bakti',
+                                'purnama_karya_bersama' => 'PT. Purnama Karya Bersama',
+                            ])
+                            ->nullable()
+                            ->native(false),
+                    ])
+                    ->query(fn ($query, $data) =>
+                        $query->when(isset($data['nama_perusahaan']), fn ($q) =>
+                            $q->where('nama_perusahaan', $data['nama_perusahaan'])
+                        )
+                    ),
+
+
+                    Filter::make('created_from')
+                    ->label('Dari Tanggal')
+                    ->form([
+                        DatePicker::make('created_from')
+                            ->label('Dari')
+                            ->displayFormat('Y-m-d'),
+                    ])
+                    ->query(fn ($query, $data) =>
+                        $query->when($data['created_from'] ?? null, fn ($q) =>
+                            $q->whereDate('created_at', '>=', $data['created_from'])
+                        )
+                    ),
+
+                Filter::make('created_until')
+                    ->label('Sampai Tanggal')
+                    ->form([
+                        DatePicker::make('created_until')
+                            ->label('Sampai')
+                            ->displayFormat('Y-m-d'),
+                    ])
+                    ->query(fn ($query, $data) =>
+                        $query->when($data['created_until'] ?? null, fn ($q) =>
+                            $q->whereDate('created_at', '<=', $data['created_until'])
+                        )
+                    ),
+
+            ], layout: FiltersLayout::AboveContent)
+            ->filtersFormMaxHeight('400px')
+            ->filtersFormColumns(3)
+            ->filtersFormWidth(MaxWidth::FourExtraLarge)
+
             ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
+                ActionGroup::make([
+                    ViewAction::make()
+                        ->color('success')
+                        ->label('Lihat'),
+                    EditAction::make()
+                        ->color('info')
+                        ->label('Ubah')
+                        ->successNotification(
+                            Notification::make()
+                                ->success()
+                                ->title('Data Kartu Kontrol Diubah')
+                                ->body('Data Kartu Kontrol telah berhasil disimpan.')),
+                        DeleteAction::make()
+                        ->color('danger')
+                        ->label(fn ($record) => "Hapus Blok {$record->siteplan}")
+                        ->modalHeading(fn ($record) => "Konfirmasi Hapus Blok{$record->siteplan}")
+                        ->modalDescription(fn ($record) => "Apakah Anda yakin ingin menghapus blok {$record->siteplan}?")
+                        ->successNotification(
+                            Notification::make()
+                                ->success()
+                                ->title('Data Kartu Kontrol Dihapus')
+                                ->body('Data Kartu Kontrol telah berhasil dihapus.')),
+                    RestoreAction::make()
+                    ->color('info')
+                    ->label(fn ($record) => "Kembalikan {$record->siteplan}")
+                    ->modalHeading(fn ($record) => "Konfirmasi Kembalikan Blok{$record->siteplan}")
+                    ->modalDescription(fn ($record) => "Apakah Anda yakin ingin mengembalikan blok {$record->siteplan}?")
+                    ->successNotification(
+                        Notification::make()
+                            ->success()
+                            ->title('Data Kartu Kontrol')
+                            ->body('Data Kartu Kontrol berhasil dikembalikan.')
+                    ),
+                    ForceDeleteAction::make()
+                    ->color('primary')
+                    ->label(fn ($record) => "Hapus Permanent {$record->siteplan}")
+                    ->modalHeading(fn ($record) => "Konfirmasi Hapus Blok Permanent{$record->siteplan}")
+                    ->modalDescription(fn ($record) => "Apakah Anda yakin ingin mengahapus blok secara permanent {$record->siteplan}?")
+                    ->successNotification(
+                        Notification::make()
+                            ->success()
+                            ->title('Data Kartu Kontrol')
+                            ->body('Data Kartu Kontrol berhasil dihapus secara permanen.')
+                    ),
+                    ])->button()->label('Action'),
+                ], position: ActionsPosition::BeforeCells)
+
+                ->groupedBulkActions([
+                    BulkAction::make('delete')
+                        ->label('Hapus')
+                        ->icon('heroicon-o-trash')
+                        ->color('danger')
+                        ->successNotification(
+                            Notification::make()
+                                ->success()
+                                ->title('Data Kartu Kontrol')
+                                ->body('Data Kartu Kontrol berhasil dihapus.'))
+                                ->requiresConfirmation()
+                        ->action(fn (Collection $records) => $records->each->delete()),
+
+                    BulkAction::make('forceDelete')
+                        ->label('Hapus Permanent')
+                        ->icon('heroicon-o-x-circle')
+                        ->color('warning')
+                        ->successNotification(
+                            Notification::make()
+                                ->success()
+                                ->title('Data Kartu Kontrol')
+                                ->body('Data Kartu Kontrol berhasil dihapus secara permanen.'))                        ->requiresConfirmation()
+                        ->action(fn (Collection $records) => $records->each->forceDelete()),
+
+                    BulkAction::make('export')
+                        ->label('Download Data')
+                        ->icon('heroicon-o-arrow-down-tray')
+                        ->color('info')
+                        ->action(fn (Collection $records) => static::exportData($records)),
+
+                    RestoreBulkAction::make()
+                        ->label('Kembalikan Data')
+                        ->icon('heroicon-o-arrow-path')
+                        ->color('success')
+                        ->button()
+                        ->successNotification(
+                            Notification::make()
+                                ->success()
+                                ->title('Data Kartu Kontrol')
+                                ->body('Data Kartu Kontrol berhasil dikembalikan.')),
+                ]);
+
+    }
+    public static function exportData(Collection $records)
+    {
+        $csvData = "ID, Nama Perusahaan, Proyek, Lokasi Proyek, Nama Konsumen, No. Telepon, Alamat, Kavling, Siteplan, Type, Luas, Tanggal Booking, Agent, Bank, Notaris, Tanggal Akad, Harga Jual, Harga/m², Pajak, Biaya Proses, Uang Muka, Estimasi KPR, Realisasi KPR, Selisih KPR, SBUM/Discount, Biaya Lain, Total Biaya, No Konsumen, Tanggal Pembayaran, Keterangan, Nilai Kontrak, Pembayaran, Sisa Saldo, Paraf, Catatan, Bukti Lainnya, Status\n";
+
+        foreach ($records as $record) {
+            $csvData .= "{$record->id}, {$record->nama_perusahaan}, {$record->proyek}, {$record->lokasi_proyek}, {$record->nama_konsumen}, {$record->no_telepon}, {$record->alamat}, {$record->kavling}, {$record->siteplan}, {$record->type}, {$record->luas}, {$record->tanggal_booking}, {$record->agent}, {$record->bank}, {$record->notaris}, {$record->tanggal_akad}, {$record->harga_jual}, {$record->harga_m}, {$record->pajak}, {$record->biaya_proses}, {$record->uang_muka}, {$record->estimasi_kpr}, {$record->realisasi_kpr}, {$record->selisih_kpr}, {$record->sbum_disct}, {$record->biaya_lain}, {$record->total_biaya}, {$record->no_konsumen}, {$record->tanggal_pembayaran}, {$record->keterangan}, {$record->nilai_kontrak}, {$record->pembayaran}, {$record->sisa_saldo}, {$record->paraf}, {$record->catatan}, {$record->bukti_lainnya}, {$record->status}\n";
+        }
+
+        return response()->streamDownload(fn () => print($csvData), 'KartuKontrolGCV.csv');
     }
 
-    public static function getRelations(): array
-    {
-        return [
-            // Tambahkan RelationManagers jika ada
-        ];
-    }
 
     public static function getPages(): array
     {
