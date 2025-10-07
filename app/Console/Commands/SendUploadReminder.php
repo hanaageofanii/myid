41df457d12e2625c06bba54bfe593bc5dc404c20
hanaageofanii <hanaageofanii@gmail.com>
2025-10-07 14:34:16 +0700

    update notification

--- app/Console/Commands/SendUploadReminder.php
+++ app/Console/Commands/SendUploadReminder.php
@@ -14,24 +14,22 @@ class SendUploadReminder extends Command
     public function handle()
     {
         $emails = [
-            'suryaniernalis440@gmail.com',
-            'izanurul4@gmail.com',
-            'pipitsaflendra11@gmail.com',
-            'atiyastuti2@gmail.com',
-            'ekaningsih122@gmail.com',
-            'malindamariaf5@gmail.com',
-            'alizapkb@gmail.com',
-            'kprgcvtkr@gmail.com',
-            'kartikasariningsih94@gmail.com',
-            'srinuroktavia201000@gmail.com',
-            'hesty3404@gmail.com',
-            'triastutihutami.99@gmail.com',
-            'hernagcv@gmail.com',
-            'zakiagcv@gmail.com',
+            // 'suryaniernalis440@gmail.com',
+            // 'izanurul4@gmail.com',
+            // 'pipitsaflendra11@gmail.com',
+            // 'atiyastuti2@gmail.com',
+            // 'ekaningsih122@gmail.com',
+            // 'malindamariaf5@gmail.com',
+            // 'alizapkb@gmail.com',
+            // 'kprgcvtkr@gmail.com',
+            // 'kartikasariningsih94@gmail.com',
+            // 'srinuroktavia201000@gmail.com',
+            // 'hesty3404@gmail.com',
+            // 'triastutihutami.99@gmail.com',
+            // 'hernagcv@gmail.com',
+            // 'zakiagcv@gmail.com',
             'hanaapkb@gmail.com',
             'gcvsystem@gmail.com',
-            '',
-            '',
         ];
 
         foreach ($emails as $email) {
